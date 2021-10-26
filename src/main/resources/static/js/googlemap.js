a8b4d92b8b839ec3cb05c7f4c4f55f70989220f3
mpazdelavega <pazmanuel671@gmail.com>
2021-10-26 00:09:59 -0300

    Se arreglo pequeño problema con Google Maps, se cambio el logo y nombre, se agrego nuevo apartado en el navbar.

--- src/main/resources/static/js/googlemap.js
+++ src/main/resources/static/js/googlemap.js
@@ -2,7 +2,7 @@
 function iniciarMap(){
     var coord = {lat:-33.4569400 ,lng: -70.6482700};
     var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
+      zoom: 20,
       center: coord
     });
     var marker = new google.maps.Marker({
